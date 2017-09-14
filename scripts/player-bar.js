7e74f23a3da8263810613fd93430c17c54a50a64
sonjasnoeyink <sonja.snoeyink@gmail.com>
2017-09-14 11:58:02 -0400

    Assignment 13: Completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -15,4 +15,15 @@ $( document ).ready(function() {
     player.playPause(nextSong);
 
   });
+
+  $('button#previous').click( function() {
+    if (player.playState !== 'playing') { return; }
+
+    const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+    const previousSongIndex = currentSongIndex - 1;
+    if (previousSongIndex < 0) { return; }
+
+    const previousSong = album.songs[previousSongIndex];
+    player.playPause(previousSong);
+  });
  });
